6a65305cc6faa1ddb422288c742020d79a5ce4f8
Martin Zehetmayer <martin.zehetmayer@maibornwolff.de>
2017-01-12 09:43:15 +0100

    Upgrade to Alpine 3.5 and Kibana 5.1.1

--- run.sh
+++ run.sh
@@ -6,11 +6,14 @@ KIBANA_CONF_FILE="/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 
 KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
 
-sed -i "s;^elasticsearch_url:.*;elasticsearch_url: ${KIBANA_ES_URL};" "${KIBANA_CONF_FILE}"
+# Dirty hack: The complete file has no uncommented line, so we write it new
+echo "# File written by run.sh" > ${KIBANA_CONF_FILE}
+echo "server.host: 0.0.0.0" >> ${KIBANA_CONF_FILE}
+echo "elasticsearch.url: ${KIBANA_ES_URL}" >> ${KIBANA_CONF_FILE}
 
 if [ -n "${KIBANA_INDEX}" ]; then
     echo "setting index!"
-    sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "${KIBANA_CONF_FILE}"
+    echo "kibana.index: ${KIBANA_INDEX}" >> ${KIBANA_CONF_FILE}
 fi
 
 # mesos-friendly change
